1527aeab2e75211f762c0b2bbfe328bb99b716eb
Vuk Djukic <vuchina@icloud.com>
2019-08-05 10:57:17 -0700

    Update ProgressBarHelper.js

--- AnimatedLightningProgressBar/ProgressBarHelper.js
+++ AnimatedLightningProgressBar/ProgressBarHelper.js
@@ -1,5 +1,11 @@
+/** 
+* $A.getCallback - ensures that the lightning components framework rerenders the markup
+*/
+
 ({
     valueChanged : function(cmp, ev) {
+        // capture the start and end values and figure out if 
+        // to increment or decrement from the current value
         var times=0;
         var current=cmp.get('v.value');
         var final=cmp.get('v.inputVal');
@@ -8,6 +14,7 @@
             increment=-1;
         }
         var self=this;
+        // timer is set up to repeat every 100 milliseconds
         var timeoutRef = window.setInterval($A.getCallback(function() {
             if (cmp.isValid()) {
                 var value=cmp.get('v.value');
